e9c7fd057eff313356a232acddacbb2ceacb05e2
Kay Zheng <l04m33@gmail.com>
2012-11-09 22:21:33 +0800

    *) populate the date cells dynamically

--- ycalendar/static/js/calendar.js
+++ ycalendar/static/js/calendar.js
@@ -16,7 +16,56 @@ function cell_on_click() {
 }
 
 
+function populate_cells() {
+    var i;
+    var cells_area = $("#cells-area");
+
+    cells_area.empty();
+    for(i = 0; i < 7; i++) {
+        var col_id = "col-" + i;
+        var new_col = $('<div class="cells-column" id="' + col_id + '" />');
+
+        cells_area.append(new_col)
+    }
+
+    var now = moment();
+    var first_day_of_month = moment().startOf("month");
+    var first_day_of_page = first_day_of_month.clone().subtract("days", first_day_of_month.day());
+    var last_day_of_month = moment().endOf("month");
+    var last_day_of_page = last_day_of_month.clone().add("days", 6 - last_day_of_month.day());
+
+    var cur_day = first_day_of_page.clone();
+    var cell_class, 
+        container, 
+        new_cell,
+        new_date_span;
+
+    while (cur_day.toDate() < last_day_of_page) {
+        switch (cur_day.day()) {
+        case 0:
+            cell_class = "left-cell";
+            break;
+        case 6:
+            cell_class = "right-cell";
+            break;
+        default:
+            cell_class = "cell";
+            break;
+        }
+
+        container = $("#col-" + cur_day.day());
+        new_cell = $('<div class="' + cell_class + '" />');
+        new_date_span = $('<span class="date">' + cur_day.date() + '</span>');
+        new_cell.append(new_date_span);
+        container.append(new_cell);
+
+        cur_day.add("days", 1);
+    }
+}
+
+
 $(document).ready(function() {
+    populate_cells();
     $("#details").hide();
     $(".cell").bind("click", cell_on_click);
     $(".left-cell").bind("click", cell_on_click);
