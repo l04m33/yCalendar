eed5934ecdf62f6d4c26bcf5551d2099d1578902
Kay Zheng <l04m33@gmail.com>
2012-11-09 09:18:59 +0800

    *) add path parameters to daily_list route

--- ycalendar/views.py
+++ ycalendar/views.py
@@ -15,12 +15,15 @@ import sqlalchemy as sqla
 # TODO: add the input date parameter and process it....
 @view_config(route_name='daily_list.json', renderer='json')
 def daily_list_json(request):
-    try:
-        info_list = DBSession.query(DetailInfo).filter(
-                DetailInfo.timestamp >= dt.datetime(2012, 11, 8, 0, 0, 0),
-                DetailInfo.timestamp < dt.datetime(2012, 11, 9, 0, 0, 0)).order_by(sqla.desc(DetailInfo.timestamp)).all()
-    except DBAPIError:
-        return Response('Datebase error.', content_type='text/plain', status_int=500)
+    year = int(request.matchdict.get('year', -1))
+    month = int(request.matchdict.get('month', -1))
+    day = int(request.matchdict.get('day', -1))
+    this_day = dt.datetime(year, month, day)
+    next_day = this_day + dt.timedelta(1, 0, 0)
+    info_list = DBSession.query(DetailInfo).filter(
+            DetailInfo.timestamp >= this_day, 
+            DetailInfo.timestamp < next_day).order_by(
+                    sqla.desc(DetailInfo.timestamp)).all()
     return [detail_info_to_dict(d) for d in info_list]
 
 
